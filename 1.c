d9e40b7d10158b7470a13a61eb21c2da252bb45f
suryakumarsivakumar <231401113@rajalakshmi.edu.in>
2024-07-29 19:46:57 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   int a,b;
+   float c,d;
+   scanf("%d %d",&a,&b);
+   printf("%d",a+b);
+   printf("%d",a-b);
+   scanf("%f %f",&c,&d);
+   printf("%f",c+d);
+   printf("%f",c-d);
+   return 0;
+}
+
